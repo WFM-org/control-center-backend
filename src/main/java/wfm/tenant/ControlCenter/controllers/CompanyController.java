64abbc0d2b110849a857e0d1490c9c3be1145ba4
Dogan <yildirim2635@hotmail.com>
2025-03-13 20:08:10 +0100

    change port and endpoint structure

--- src/main/java/wfm/tenant/ControlCenter/controllers/CompanyController.java
+++ src/main/java/wfm/tenant/ControlCenter/controllers/CompanyController.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/company")
+@RequestMapping("/cc/company")
 @RequiredArgsConstructor
 public class CompanyController {
 
--- src/main/java/wfm/tenant/ControlCenter/controllers/LanguagePackController.java
+++ src/main/java/wfm/tenant/ControlCenter/controllers/LanguagePackController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/languagePack")
+@RequestMapping("/cc/languagePack")
 @RequiredArgsConstructor
 @Slf4j
 public class LanguagePackController {
